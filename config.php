387b45fd574549290dec289c6c741e4c600de8aa
talentelgia <mgmt@talentelgia.in>
2019-11-06 17:00:18 +0530

    code pull

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
-
 define('DB_NAME', 'crew_app_development');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PWD', 'root');
-
- ?>
+define('DB_PWD', 'psFk6~PjxG5=(FH-Z');
